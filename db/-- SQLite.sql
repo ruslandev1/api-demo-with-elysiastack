aa40530374932e5ae653d9b05b2d69c35f02fdb2
ruslandev1 <ruslanqurbanovdev@gmail.com>
2023-11-07 16:46:35 +0400

    i started bun+hono api stack for generating books api
    this is my first backend project and i use bun+sqlite+hono
    God,Help me :d

---
+++ db/-- SQLite.sql
@@ -0,0 +1,8 @@
+-- SQLite
+CREATE TABLE books(
+  id integer NOT NULL,
+  name text NOT NULL,
+  author text NOT NULL,
+  booktype text NOT NULL,
+  length integer NOT NULL
+);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,36 @@
-import { Elysia } from "elysia";
+import { Hono } from "hono";
+import { Database } from "bun:sqlite";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const db = new Database("booksdb.sqlite");
+db.exec("PRAGMA journal_mode = WAL;");
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+db.run(
+  "CREATE TABLE IF NOT EXISTS books (id INTEGER name TEXT author TEXT booktype TEXT length INTEGER)"
 );
+// db.run(
+//   `INSERT INTO books VALUES (1, 'The Gambler', 'Fyodor Dostoevsky', 'novella', 197)`
+// );
+
+const app = new Hono();
+
+const port = 3001;
+
+const home = app.get("/", (c) => {
+  return c.json({ message: "go finger yourself and sniff!" });
+});
+
+app.get("/api/v1/books", (c) => {
+  const stmt = db.prepare("SELECT * FROM books");
+  return c.json({
+    status: 200,
+    success: true,
+    message: "Hello Hono!",
+    books: stmt.all(),
+  });
+});
+
+console.log(`This app running on https://localhost:${port}`);
+export default {
+  port,
+  fetch: home.fetch,
+};
